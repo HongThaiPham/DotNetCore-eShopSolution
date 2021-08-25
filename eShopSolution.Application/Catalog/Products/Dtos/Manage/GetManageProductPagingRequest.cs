9c8c571d4742bf6099a540525fe539b0d99f6505
Pham Hong Thai <hongthaipro@gmail.com>
2021-08-25 10:14:34 +0700

    fix conflic after merge feature/application_layer

---
+++ eShopSolution.Application/Catalog/Products/Dtos/Manage/GetManageProductPagingRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using eShopSolution.Application.Dtos;
+
+namespace eShopSolution.Application.Catalog.Products.Dtos.Manage
+{
+    public class GetManageProductPagingRequest : PagingRequestBase
+    {
+        public string Keyword { get; set; }
+
+        public List<int> CategoryIds { get; set; }
+    }
+}
