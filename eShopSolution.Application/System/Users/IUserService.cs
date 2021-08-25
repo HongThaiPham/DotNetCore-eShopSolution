adacf0b418ce8f2f37eac19793ca58e165f90556
Pham Hong Thai <hongthaipro@gmail.com>
2021-08-25 13:21:08 +0700

    Create login and register APIs

---
+++ eShopSolution.Application/System/Users/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+using eShopSolution.ViewModels.System.Users;
+
+namespace eShopSolution.Application.System.Users
+{
+    public interface IUserService
+    {
+        Task<string> Authencate(LoginRequest request);
+
+        Task<bool> Register(RegisterRequest request);
+    }
+}
